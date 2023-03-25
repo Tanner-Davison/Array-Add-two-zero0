1ac2addb3cf4b9b01cb41b76056df0d1b81526fe
tannerdavison <tboydavison@gmail.com>
2023-03-25 16:18:44 -0600

    DoubleForLoop and nested function

---
+++ addToZero.js
@@ -0,0 +1,37 @@
+// Starting array
+let array = [28, 43,-12, 30, 4, 0, 12]
+
+// Write your solution below:
+
+
+
+function addTwoZero (addInArray){
+    const seenNumbers = {};
+    for(let num of addInArray){
+        if(-num in seenNumbers){
+        return true;
+        
+    }else {
+        seenNumbers[num]= true;
+    }
+}
+console.log(false)
+}
+
+console.log(addTwoZero(array))
+
+//ORRRRR YOU CAN WRITE IT WITH A NESTED FOR LOOP
+
+console.log(addTwoZero(array))
+
+let answer = false;
+
+for(i=0; i<array.length;i++){
+
+    for(j=0; j<array.length;j++){
+        if(array[j]+array[i]===0){
+            answer =true;
+        }
+    }
+}
+console.log(answer)
